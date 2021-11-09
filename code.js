85e7419ef1f41e6464c700e398db210a59b3e3c3
Nielsnlamf <nielsnlamf@gmail.com>
2021-11-09 12:45:44 +0100

    Started working on the files for the electron application.

---
+++ code.js
@@ -0,0 +1,10 @@
+function move(button) {
+    console.log(button.id + " was called")
+}
+
+window.onload=function(){
+    document.querySelectorAll('.movement').forEach(item => {
+        item.addEventListener('click', () => {
+            move(item)
+        })
+})}
